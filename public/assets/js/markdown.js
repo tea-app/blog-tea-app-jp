338cfb45363763dd564eb9b79eafbf26fb217df5
iRosSoftware <iros0522@gmail.com>
2018-09-07 03:16:33 +0900

    markdowntest

--- public/assets/js/markdown.js
+++ public/assets/js/markdown.js
@@ -1,6 +1,30 @@
 <script src="http://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js"></script>
 
+
+gethtmltext(getmarkdowntext());
+
 function gethtmltext(markdowntext) {
     var md = marked(markdowntext);
     return md;
+}
+
+function getmarkdowntext() {
+    var id = getParam('id');
+    return "#Hello";
+}
+
+/**
+ * Get the URL parameter value
+ *
+ * @param  name {string} パラメータのキー文字列
+ * @return  url {url} 対象のURL文字列（任意）
+ */
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
