1bf2867a4db362b8ab74a73fc051a73759916144
iRosSoftware <iros0522@gmail.com>
2018-09-08 17:49:41 +0900

    blog lastest

--- public/assets/js/markdown.js
+++ public/assets/js/markdown.js
@@ -1,29 +1,39 @@
 window.onload = function () {
     var sync = function () {
-        getcontentsinfiletext("info.json").then(function onFulfilled(value) {
+        var id;
+        getcontentslastestPromise().then(function onFulfilled(value) {
             console.log(value);
-            var title = getjsontotitle(value); // マークダウンに変換
-            var author = getjsontoauthor(value); // マークダウンに変換
-            document.getElementById("blog-title").innerHTML = title;
-            document.getElementById("blog-author").innerHTML = author;
-
+            id = value;
+            return getcontentsinfiletextPromise("info.json", id);
         }).catch(function onRejected(error) {
             console.error(error);
-            document.getElementById("blog-title").innerHTML = "error";
-            document.getElementById("blog-author").innerHTML = "error";
-        });
+        })
+            //get blog info
+            .then(function (result1) {
+                console.log(result1);
+                var title = getjsontotitle(result1); // マークダウンに変換
+                var author = getjsontoauthor(result1); // マークダウンに変換
+                document.getElementById("blog-title").innerHTML = title;
+                document.getElementById("blog-author").innerHTML = author;
+                return getcontentsinfiletextPromise("main.md", id);
+            })
+            .catch(function onRejected(error) {
+                console.error(error);
+                document.getElementById("blog-title").innerHTML = "error";
+                document.getElementById("blog-author").innerHTML = "error";
+            })
 
-        getcontentsinfiletext("main.md").then(function onFulfilled(value) {
-            console.log(value);
-            var md = gethtmltext(value); // マークダウンに変換
-            document.getElementById("blog-contents").innerHTML = md;
-
-        }).catch(function onRejected(error) {
-            console.error(error);
-            document.getElementById("blog-contents").innerHTML = error;
-        });
+            //get blog contents
+            .then(function (result2) {
+                console.log(result2);
+                var md = gethtmltext(result2); // マークダウンに変換
+                document.getElementById("blog-contents").innerHTML = md;
+            })
+            .catch(function onRejected(error) {
+                console.error(error);
+                document.getElementById("blog-contents").innerHTML = error;
+            })
     };
-    // 一回やっとく
     sync();
 };
 
@@ -41,34 +51,6 @@ function getjsontotitle(json) {
 function getjsontoauthor(json) {
     var data = JSON.parse(json);
     return data.author;
-}
-
-
-function getcontentsinfiletext(filename) {
-    return new Promise((resolve, reject) => {
-        var id = getParam('id');
-        if (id == null) {
-            id = getlastestid();
-        }
-        var url = "/blog/contents/" + id + "/" + filename;
-        var request = createXMLHttpRequest();
-        request.open("GET", url);
-
-        request.addEventListener("load", (event) => {
-            if (event.target.status !== 200) {
-                console.log(`${event.target.status}: ${event.target.statusText}`);
-            }
-            resolve(event.target.responseText);
-        });
-
-        request.addEventListener("error", () => {
-            reject(new Error(request.statusText));
-            console.error("Network Error");
-        });
-        request.overrideMimeType('text/plain; charset=UTF-8');  //強制Text
-
-        request.send();
-    });
 }
 
 function createXMLHttpRequest() {
@@ -97,7 +79,7 @@ function getParam(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function getMarkdownFile(id) {
+function getMarkdownFile() {
     var f = new Folder(folderPath);
     f.next();
 
@@ -119,20 +101,43 @@ function getMarkdownFile(id) {
     f.close();
 }
 
-function getlastestid() {
-    getcontentslastest().then(function onFulfilled(value) {
-        console.log(value);
-        var arr = JSON.parse(value);
-        arr = arr.sort();
-        return arr[0];
-    }).catch(function onRejected(error) {
-        console.error(error);
+//IDが指定されていなければ、最新の記事のIDを返す　指定されていればそれを返す
+function getcontentslastestPromise() {
+    return new Promise((resolve, reject) => {
+        var id = getParam('id');
+        if (id == null) {
+            console.log("Get Lastest ID");
+            var url = "/blog/contents/contents.json"
+            var request = createXMLHttpRequest();
+            request.open("GET", url);
+
+            request.addEventListener("load", (event) => {
+                if (event.target.status !== 200) {
+                    console.log(`${event.target.status}: ${event.target.statusText}`);
+                }
+                var arr = JSON.parse(event.target.responseText);
+                arr = arr.sort();
+                var lastid = arr[arr.length - 1];
+                resolve(lastid);
+            });
+
+            request.addEventListener("error", () => {
+                reject(new Error(request.statusText));
+                console.error("Network Error");
+            });
+            request.overrideMimeType('text/plain; charset=UTF-8');  //強制Text
+
+            request.send();
+        }
+        else {
+            resolve(id);
+        }
     });
 }
 
-function getcontentslastest() {
+function getcontentsinfiletextPromise(filename, id) {
     return new Promise((resolve, reject) => {
-        var url = "/blog/contents.json"
+        var url = "/blog/contents/" + id + "/" + filename;
         var request = createXMLHttpRequest();
         request.open("GET", url);
 
